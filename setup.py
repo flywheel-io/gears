941076a059fd6bb1226d7b96f423a2da8633971f
Gunnar Schaefer <gsfr@stanford.edu>
2016-10-17 16:47:38 -0700

    Add cli entry point

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    version      = '0',
-    name         = 'gears',
-    author       = 'Flywheel Exchange, LLC',
+    version = '0',
+    name = 'gears',
+    author = 'Flywheel Exchange, LLC',
     author_email = 'support@flywheel.io',
     description  = 'Flywheel Gear tools',
-    url          = 'https://github.com/flywheel-io/gears',
+    url = 'https://github.com/flywheel-io/gears',
 
-    packages     = ['gears'],
+    packages = ['gears'],
     package_data = {'gears': ['../spec/manifest.schema.json']},
     install_requires = ['jsonschema', 'rfc3987'],
 )
